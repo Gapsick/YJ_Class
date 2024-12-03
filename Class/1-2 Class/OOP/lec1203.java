feced665bfc775990b430b05c03a696387d703b8
Gapsick <gapsicke@gmail.com>
2024-12-03 12:55:04 +0900

    add folder

---
+++ Class/1-2 Class/OOP/lec1203.java
@@ -0,0 +1,15 @@
+public class lec1203 {
+    static int getSum(int argA, int argB) {
+        return argA + argB;
+    }
+
+    static void saySomthing(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getSum(2, 3));
+        saySomthing();
+    }
+}
