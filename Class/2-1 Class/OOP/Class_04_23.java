9e14068a63648cda922192d0711dc5875645c73a
Gapsick <gapsicke@gmail.com>
2025-04-23 15:51:33 +0900

    add Abstract 25.04.23 class

---
+++ Class/2-1 Class/OOP/Class_04_23.java
@@ -0,0 +1,22 @@
+
+abstract class Animal {
+    abstract void makeSound();
+
+}
+
+abstract class Dog extends Animal {
+
+}
+
+class Son extends Dog {
+    void makeSound() {}
+}
+
+
+public class Class_04_23 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
