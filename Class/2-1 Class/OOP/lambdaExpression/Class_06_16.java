f5e46364a526a371ed9c9a4bd83181fdbf7430c9
Gapsick <gapsicke@gmail.com>
2025-06-21 22:11:24 +0900

    add java class

--- Class/2-1 Class/OOP/lambdaExpression/Class_06_16.java
+++ Class/2-1 Class/OOP/lambdaExpression/Class_06_16.java
@@ -1,4 +1,27 @@
 package lambdaExpression;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+class Person {
+    String name;
+    int age;
+    Person(String name, int age) { this.name = name; this.age = age; }
+}
 
 public class Class_06_16 {
+    public static void main(String[] args) {
+
+        // Collection Framework : package for Data Structure
+        ArrayList<Person> list = new ArrayList<>();
+
+        list.add(new Person("John", 20));
+        list.add(new Person("Jane", 31));
+        list.add(new Person("Jack", 41));
+        list.add(new Person("Jill", 51));
+
+        Consumer<Person> prt = (obj) -> System.out.println(obj.name + ":" + obj.age);
+
+        list.stream().filter((obj) -> obj.age > 30).forEach(prt);
+
+    }
 }
--- Class/2-1 Class/OOP/lambdaExpression/Class_06_16_2.java
+++ Class/2-1 Class/OOP/lambdaExpression/Class_06_16_2.java
@@ -1,4 +1,25 @@
 package lambdaExpression;
 
+class Add<T, R> {
+    R op (T a, T b) { return null; }
+}
+
+
 public class Class_06_16_2 {
+    public static void main(String[] args) {
+
+        // Wrapper classes : Integer, Float, Double ...
+        // Primity datatype을 객체로 만들고, refer class라고 함
+
+        Integer myValue = 10;   // Auto-boxing
+//        -> Integer myValue = new Integer(10);
+
+
+        int myAge = myValue;    // Unboxing
+
+
+
+
+
+    }
 }
